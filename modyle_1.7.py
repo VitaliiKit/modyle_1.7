86f7c12f8663fcf34b4f208946f8fc9b141431a5
VitaliiKit <welld-94@mail.ru>
2024-09-22 23:15:54 +0300

    Add files via upload

---
+++ modyle_1.7.py
@@ -0,0 +1,17 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+A=grades [0]
+Aaron= sum(A)/ len(A)
+print('Aaron :', Aaron)
+B= grades [1]
+Bilbo= sum(B)/ len(B)
+print('Bilbo :', Bilbo)
+J= grades [2]
+Johnny= sum(J)/ len(B)
+K= grades [3]
+Khendrik= sum(K)/ len(K)
+S= grades [4]
+Steve= sum(S)/ len(S)
+print(Aaron, Bilbo, Johnny, Khendrik, Steve)
+students_= {'Aaron': Aaron, 'Bilbo': Bilbo, 'Steve': Steve, 'Khendrik': Khendrik, 'Johnny': Johnny}
+print(students_)
